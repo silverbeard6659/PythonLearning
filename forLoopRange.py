636825ca508deb63082efd29cccdc5de0535c031
silverbeard6659 <silverbeard665.9@gmail.com>
2024-12-03 19:33:51 +0300

    changed file

--- forLoopRange.py
+++ forLoopRange.py
@@ -1,1 +1,15 @@
-print("new file")
+numbers = [52,32,56,753,634,64,75,3,2,6,234,7,32,4,32,634,22,1,3,123,1523,2367,33,43,23,23]
+random_numbers=[]
+even_numbers=[]
+count=10
+i=0
+while i < count:
+    random_numbers.append(randint(1,1000))
+    i += 1
+print(random_numbers)
+for number in random_numbers:
+    if number%2 == 0:
+        even_numbers.append(number)
+        continue
+    print(number)
+print(even_numbers)
